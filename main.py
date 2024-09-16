da503b61b8f4ea6048feda3a87c2d4505c3f4f60
EvGeniusM <thezhekychan@gmail.com>
2024-09-16 20:41:39 +0500

    experimenting

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print(1)
+import typer
+import requests
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(url: str):
+    request = requests.get(url)
+    print(request.status_code)
+    print(request.headers)
+    print(request.text)
+
+
+@app.command()
+def goodbye():
+    print("bye")
+
+
+if __name__ == "__main__":
+    app()
