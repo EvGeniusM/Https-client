ca939d51b7899185d7945e05c1367ca18792166e
EvGeniusM <thezhekychan@gmail.com>
2024-09-16 21:45:50 +0500

    added-get-command

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import typer
 import requests
+import json
+from os import path, makedirs
 
 app = typer.Typer()
 
 
 @app.command()
-def hello(url: str):
+def get(url: str, save=False):
     request = requests.get(url)
-    print(request.status_code)
-    print(request.headers)
-    print(request.text)
+    if save:
+        dictionary = {
+            'url': request.url,
+            'request method': 'get',
+            'timeout': str(request.elapsed),
+            'headers': dict(request.headers),
+        }
 
+        if not path.exists('json'):
+            makedirs('json')
 
-@app.command()
-def goodbye():
-    print("bye")
+        with open('json/saved_contents.json', 'w') as f:
+            json.dump(dict(dictionary), f)
+            print(save)
+    else:
+        print("I did nothing")
 
 
 if __name__ == "__main__":
